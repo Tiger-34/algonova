442b435dd140f57819ee173197a297ee6d33236a
maxmachac <max.machac@icloud.com>
2026-04-21 19:44:53 +0200

    main

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,4 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import heroImg from './assets/hero.png'
-  import Counter from './lib/Counter.svelte'
+  
 </script>
 
