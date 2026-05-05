0a7728afc8dd0ca2e9bfac413379058277f6d4d6
maxmachac <max.machac@icloud.com>
2026-05-05 19:22:59 +0200

    main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
-
+	import Overlay from '../components/ui/overlay.svelte';
+	import {isOverlayOpen} from '../stores/overlay.js';
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+{#if $isOverlayOpen}
+	<Overlay/>
+{/if}
 
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script>
-// @ts-nocheck
-
-    import RecipeIcon from "../lib/assets/RecipeIcon.svelte";
-
-    import { recipes } from "../lib/assets/shared.svelte";
+    import PopupBtn from '../components/ui/overlaybtn.svelte';
+    import Card from '../components/ui/card.svelte';
 </script>
 
-{#each recipes.info as recipe}
-    <RecipeIcon {...recipe}/>
-{/each}
+<Card/>
+<PopupBtn/>
+
+
 
