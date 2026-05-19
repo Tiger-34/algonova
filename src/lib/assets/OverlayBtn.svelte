8a1d031338c7e9bf7e5de64b9c4eab75281c934c
Tiger34 <nowotniansky@gmail.com>
2026-05-19 17:20:37 +0200

    adding header, styling main page, adding remove function

--- src/lib/assets/OverlayBtn.svelte
+++ src/lib/assets/OverlayBtn.svelte
@@ -2,5 +2,37 @@
     import { isOverlayOpen } from "./shared.svelte";
 </script>
 
-{isOverlayOpen.value}
-<button on:click={() => {isOverlayOpen.value = !isOverlayOpen.value}}>Open Overlay</button>
+
+<!-- svelte-ignore a11y_consider_explicit_label -->
+<button on:click={() => {isOverlayOpen.value = !isOverlayOpen.value}}>
+    <div></div>
+</button>
+
+<style>
+
+    button {
+        height: 40px;
+        width: 40px;
+
+        background-color: #ffffff;
+
+        border: 0px solid #ffffff;
+        border-radius: 50%;
+
+        cursor: pointer;
+    }
+        button:hover {
+            background-color: #cacaca;
+        }
+        button:active {
+            background-color: #ababab;
+        }
+
+    div:after {
+        font-size: xx-large;
+
+        content: "\002b";
+
+        margin: auto;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,70 @@
     import RecipeIcon from "$lib/assets/RecipeIcon.svelte";
 
     import Overlay from "$lib/assets/Overlay.svelte";
-    import Overlaybtn from "$lib/assets/OverlayBtn.svelte";
+    import OverlayBtn from "$lib/assets/OverlayBtn.svelte"
 
     import { recipes } from "$lib/assets/shared.svelte";
 
     import { isOverlayOpen } from "$lib/assets/shared.svelte";
-    import OverlayBtn from "$lib/assets/OverlayBtn.svelte";
+
+    import { adding_info } from "$lib/assets/shared.svelte";
+
 </script>
 
-{#each recipes.info as recipe (recipe.id)}
-    <RecipeIcon {...recipe}/>
-{/each}
+<div id="parent">
 
-<OverlayBtn />
+    <header>
+        <input id="search_add_bar" type="text" bind:value={adding_info.name} placeholder="Vyhľadať">
 
-{#if isOverlayOpen.value}
-    <Overlay />
-{/if}
+        <OverlayBtn />
+    </header>
+
+    <div id="recipes">
+        {#each recipes.info as recipe (recipe.id)}
+            <RecipeIcon {...recipe}/>
+        {/each}
+    </div>
+
+    {#if isOverlayOpen.value}
+        <Overlay />
+    {/if}
+
+</div>
+
+<style>
+
+    #recipes {
+        display: flex;
+        flex-wrap: wrap;
+        flex-shrink: 1;
+        justify-content: space-around;
+
+        margin: 10px;
+    }
+
+    header {
+        height: 60px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        border-radius: 10px;
+        
+        background-color: #2cff24;
+        
+    }
+
+    #search_add_bar {
+        width: 190px;
+        height: 30px;
+
+        border: 5px solid #ffffff;
+        border-radius: 10px;
+
+        margin: 25px;
+
+        font-size: large;
+    }
+
+</style>
