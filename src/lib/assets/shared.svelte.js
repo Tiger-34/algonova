c3b8b42595a738b6cba51fc91126116e90239710
Tiger34 <nowotniansky@gmail.com>
2026-05-20 19:49:10 +0200

    removed unnecessery variable adding_info

--- src/lib/assets/shared.svelte.js
+++ src/lib/assets/shared.svelte.js
@@ -6,17 +6,6 @@ export let recipes = $state({
 
 export let isOverlayOpen = $state({value: false});
 
-export let adding_info = $state({
-    new_recipe_info: {
-        id: recipes.current_id,
-        name: "",
-        description: [],
-        ingredients: [],
-        cook_time: 0,
-        image_url: ""
-    }
-});
-
 export let currently_showing = $state({
     value:
         {id: 1,
